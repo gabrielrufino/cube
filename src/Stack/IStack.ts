1944a5474a902d5b5923a7f06534efab44323844
Gabriel Rufino <contato@gabrielrufino.com>
2021-11-03 23:20:54 -0300

    push and pop

--- src/Stack/IStack.ts
+++ src/Stack/IStack.ts
@@ -1,6 +1,6 @@
 interface IStack<T> {
 	push(_element: T): T;
-	pop(_element: T): T;
+	pop(): T | undefined;
 }
 
 export default IStack;
--- src/Stack/index.ts
+++ src/Stack/index.ts
@@ -1,7 +1,18 @@
-import DataStructure from '../../build/DataStructure';
+import DataStructure from '../DataStructure';
+import IStack from './IStack';
 
-export default class Stack<T = number> extends DataStructure<T> {
+export default class Stack<T = number> extends DataStructure<T> implements IStack<T> {
 	constructor(...inputs: T[]) {
 		super(inputs);
+	}
+
+	public push(element: T) {
+		this._data.push(element);
+		return element;
+	}
+
+	public pop(): T | undefined {
+		const element = this._data.pop();
+		return element;
 	}
 }

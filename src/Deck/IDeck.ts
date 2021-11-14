3bd6406d0b0764838a1d191e010a8ebc117f4cbb
Gabriel Rufino <contato@gabrielrufino.com>
2021-11-13 23:57:44 -0300

    Deck

---
+++ src/Deck/IDeck.ts
@@ -0,0 +1,10 @@
+interface IDeck<T> {
+    addFront(_element: T): T;
+    addBack(_element: T): T;
+    removeFront(): T | undefined;
+    removeBack(): T | undefined;
+    peekFront(): T | undefined;
+    peekBack(): T | undefined;
+}
+
+export default IDeck;

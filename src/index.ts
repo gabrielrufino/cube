f028b4f68c43ac45d271fe41080f510d5b439338
Gabriel Rufino <contato@gabrielrufino.com>
2021-11-15 22:47:48 -0300

    Exposing Queue structure

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export {default as Algorithms} from './Algorithms';
 export {default as Array} from './Array';
 export {default as Deck} from './Deck';
+export {default as Queue} from './Queue';
 export {default as Stack} from './Stack';

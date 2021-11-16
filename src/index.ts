b71a8181e560a2d7e28d5f64e2b18c5711c1723f
Gabriel Rufino <contato@gabrielrufino.com>
2021-11-15 22:53:42 -0300

    Alias 'List' for Array

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export {default as Algorithms} from './Algorithms';
 export {default as Array} from './Array';
 export {default as Deck} from './Deck';
+export {default as List} from './List';
 export {default as Queue} from './Queue';
 export {default as Stack} from './Stack';

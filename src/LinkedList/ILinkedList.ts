5b8986388b0ba8f6049b5b2bb8d1562f7b8e12ce
Gabriel Rufino <contato@gabrielrufino.com>
2021-11-17 08:57:09 -0300

    Method 'removeFromPosition'

--- src/LinkedList/ILinkedList.ts
+++ src/LinkedList/ILinkedList.ts
@@ -7,6 +7,8 @@ interface ILinkedList<T> {
 	get data(): ILinkedListDataItem<T>[];
 	get size(): number;
 	push(_element: T): T;
+	remove(_element: T): T | undefined;
+	removeFromPosition(_position: number): T | undefined;
 }
 
 export default ILinkedList;
--- src/LinkedList/index.spec.ts
+++ src/LinkedList/index.spec.ts
@@ -70,5 +70,90 @@ describe('LinkedList', () => {
 
 			expect(returned).toBe(1);
 		});
+
+		describe('.remove()', () => {});
+
+		describe('.removeFromPosition()', () => {
+			it('Should return undefined when given a negative position', () => {
+				const linkedList = new LinkedList(1, 2, 3, 4);
+				const returned = linkedList.removeFromPosition(-1);
+
+				expect(returned).toBeUndefined();
+			});
+
+			it('Should return undefined when given a position equal size', () => {
+				const linkedList = new LinkedList(1, 2, 3, 4);
+				const returned = linkedList.removeFromPosition(linkedList.size);
+
+				expect(returned).toBeUndefined();
+			});
+
+			it('Should return undefined when given a position larger than size', () => {
+				const linkedList = new LinkedList(1, 2, 3, 4);
+				const returned = linkedList.removeFromPosition(linkedList.size + 1);
+
+				expect(returned).toBeUndefined();
+			});
+
+			it('Should remove item from the head', () => {
+				const linkedList = new LinkedList(1, 2, 3, 4);
+				linkedList.removeFromPosition(0);
+
+				expect(linkedList.data).toEqual([
+					{
+						value: 2,
+						next: 3,
+					},
+					{
+						value: 3,
+						next: 4,
+					},
+					{
+						value: 4,
+						next: null,
+					},
+				]);
+			});
+
+			it('Should remove item from the middle of the linked list', () => {
+				const linkedList = new LinkedList(1, 2, 3, 4);
+				linkedList.removeFromPosition(2);
+
+				expect(linkedList.data).toEqual([
+					{
+						value: 1,
+						next: 2,
+					},
+					{
+						value: 2,
+						next: 4,
+					},
+					{
+						value: 4,
+						next: null,
+					},
+				]);
+			});
+
+			it('Should remove item from the last position of the linked list', () => {
+				const linkedList = new LinkedList(1, 2, 3, 4);
+				linkedList.removeFromPosition(linkedList.size - 1);
+
+				expect(linkedList.data).toEqual([
+					{
+						value: 1,
+						next: 2,
+					},
+					{
+						value: 2,
+						next: 3,
+					},
+					{
+						value: 3,
+						next: null,
+					},
+				]);
+			});
+		});
 	});
 });

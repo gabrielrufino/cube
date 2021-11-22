c0c9472863911b377a3b8a5831c41f24449ce4d0
Gabriel Rufino <contato@gabrielrufino.com>
2021-11-22 07:42:21 -0300

    Method positionOf

--- src/LinkedList/ILinkedList.ts
+++ src/LinkedList/ILinkedList.ts
@@ -8,6 +8,7 @@ interface ILinkedList<T> {
 	get size(): number;
 	push(_element: T): T;
 	getFromPosition(_position: number): ILinkedListItem<T> | undefined;
+	positionOf(_element: T): number | undefined;
 	insertInPosition(_element: T, _position: number): T | undefined;
 	remove(_element: T): T | undefined;
 	removeFromPosition(_position: number): T | undefined;

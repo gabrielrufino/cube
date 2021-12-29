8e2b48e8d3e2b4f7fa24b38c9a259fdac15a167a
Gabriel Rufino <contato@gabrielrufino.com>
2021-12-28 21:28:07 -0300

    Method .extract and getter .isEmpty

--- src/MaxHeap/IMaxHeap.ts
+++ src/MaxHeap/IMaxHeap.ts
@@ -1,7 +1,9 @@
 interface IMaxHeap<T> {
     get data(): T[];
     get size(): number;
+    get isEmpty(): boolean;
     insert(_value: T): T;
+    extract(): T | null;
 }
 
 export default IMaxHeap;
--- src/MaxHeap/index.spec.ts
+++ src/MaxHeap/index.spec.ts
@@ -20,6 +20,22 @@ describe('MaxHeap', () => {
 		expect(maxHeap.size).toBe(4);
 	});
 
+	describe('.isEmpty', () => {
+		it('Should be true when the heap has no values', () => {
+			const maxHeap = new MaxHeap();
+
+			expect(maxHeap.isEmpty).toBe(true);
+		});
+
+		it('Should be false when the heap has at least one value', () => {
+			const maxHeap = new MaxHeap({
+				inputs: [faker.datatype.number()],
+			});
+
+			expect(maxHeap.isEmpty).toBe(false);
+		});
+	});
+
 	describe('.insert()', () => {
 		it('Should insert new values in the heap', () => {
 			const maxHeap = new MaxHeap();
@@ -47,6 +63,34 @@ describe('MaxHeap', () => {
 			const returned = maxHeap.insert(value);
 
 			expect(returned).toBe(value);
+		});
+	});
+
+	describe('.extract()', () => {
+		it('Should extract the max value of the heap and adjust the heap after this', () => {
+			const maxHeap = new MaxHeap({
+				inputs: [9, 8, 7, 6, 5, 4, 3, 2, 1],
+			});
+			maxHeap.extract();
+
+			expect(maxHeap.data).toEqual([8, 6, 7, 2, 5, 4, 3, 1]);
+			expect(maxHeap.size).toBe(8);
+		});
+
+		it('Should return the extracted value', () => {
+			const inputs = new Array(10).fill(undefined).map(faker.datatype.number);
+			const maxHeap = new MaxHeap({inputs});
+			const returned = maxHeap.extract();
+
+			expect(returned).toBe(Math.max(...inputs));
+		});
+
+		it('Should change nothing if the heap is empty', () => {
+			const maxHeap = new MaxHeap();
+			maxHeap.extract();
+
+			expect(maxHeap.data).toEqual([]);
+			expect(maxHeap.size).toBe(0);
 		});
 	});
 });

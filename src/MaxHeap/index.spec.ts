9d5c5237f86ce73222ec9667b21ef2e011ee1942
Gabriel Rufino <contato@gabrielrufino.com>
2021-12-28 21:10:36 -0300

    Tests for method .insert

--- src/MaxHeap/index.spec.ts
+++ src/MaxHeap/index.spec.ts
@@ -1,4 +1,5 @@
 import {describe, it, expect} from '@jest/globals';
+import faker from 'faker';
 
 import MaxHeap from './';
 
@@ -17,5 +18,35 @@ describe('MaxHeap', () => {
 
 		expect(maxHeap.data).toEqual([9, 5, 1, 2]);
 		expect(maxHeap.size).toBe(4);
+	});
+
+	describe('.insert()', () => {
+		it('Should insert new values in the heap', () => {
+			const maxHeap = new MaxHeap();
+
+			maxHeap.insert(5);
+			expect(maxHeap.data).toEqual([5]);
+			expect(maxHeap.size).toBe(1);
+
+			maxHeap.insert(2);
+			expect(maxHeap.data).toEqual([5, 2]);
+			expect(maxHeap.size).toBe(2);
+
+			maxHeap.insert(1);
+			expect(maxHeap.data).toEqual([5, 2, 1]);
+			expect(maxHeap.size).toBe(3);
+
+			maxHeap.insert(9);
+			expect(maxHeap.data).toEqual([9, 5, 1, 2]);
+			expect(maxHeap.size).toBe(4);
+		});
+
+		it('Should return the inserted value', () => {
+			const maxHeap = new MaxHeap();
+			const value = faker.datatype.number();
+			const returned = maxHeap.insert(value);
+
+			expect(returned).toBe(value);
+		});
 	});
 });

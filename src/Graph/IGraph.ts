5963aabe88a4eb250607d75e396ae1b9eb83fb6e
Gabriel Rufino <contato@gabrielrufino.com>
2021-12-29 23:57:03 -0300

    Update

--- src/Graph/IGraph.ts
+++ src/Graph/IGraph.ts
@@ -5,7 +5,9 @@ interface IGraph {
 	get nodes(): string[];
 	get edges(): [string, string][];
 	insert(_node: string): string | null;
+	// Remove(_node: string): string | null;
 	connect(_node1: string, _node2: string): [string, string];
+	// Disconnect(_node1: string, _node2: string): [string, string] | null;
 	breadthFirstSearch(_startNode: string, _callback: (_node: string) => void): void;
 	getDistancesFrom(_node: string): { [key: string]: number };
 }
